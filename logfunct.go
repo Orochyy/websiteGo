cde00782f5c8741c88d0b4fb8fe28db1512de0ed
Orochyy <yura333619@gmail.com>
2021-05-28 21:50:12 +0300

    !!

--- logfunct.go
+++ logfunct.go
@@ -9,9 +9,52 @@ func slices() {
 }
 
 func maps() {
-	x := make(map[string]int)
-	x["key"] = 10
-	fmt.Println(x["key"])
+	elements := map[string]map[string]string{
+		"H": map[string]string{
+			"name":  "Hydrogen",
+			"state": "gas",
+		},
+		"He": map[string]string{
+			"name":  "Helium",
+			"state": "gas",
+		},
+		"Li": map[string]string{
+			"name":  "Lithium",
+			"state": "solid",
+		},
+		"Be": map[string]string{
+			"name":  "Beryllium",
+			"state": "solid",
+		},
+		"B": map[string]string{
+			"name":  "Boron",
+			"state": "solid",
+		},
+		"C": map[string]string{
+			"name":  "Carbon",
+			"state": "solid",
+		},
+		"N": map[string]string{
+			"name":  "Nitrogen",
+			"state": "gas",
+		},
+		"O": map[string]string{
+			"name":  "Oxygen",
+			"state": "gas",
+		},
+		"F": map[string]string{
+			"name":  "Fluorine",
+			"state": "gas",
+		},
+		"Ne": map[string]string{
+			"name":  "Neon",
+			"state": "gas",
+		},
+	}
+
+	if el, ok := elements["Ne"]; ok {
+		fmt.Println(el["name"], el["state"])
+	}
 }
 
 func main() {
