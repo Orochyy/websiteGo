97c9d7891947950e5d9a73c8d6786287ca8afb7d
Orochyy <yura333619@gmail.com>
2021-07-26 12:46:08 +0300

    educ

--- logfunct.go
+++ logfunct.go
@@ -180,4 +180,4 @@ func main() {
 	mnoj()
 }
 
-///qq~~13reqqsaqnqnqaqvqeqrqesqhqkqo~~oqrqoqcqhqy~~iqlqoqvqeq~~yqoquqsqoqdawdvdawv
+///qq~~13reqqsaqnqnqaqvqeqrqesqhqkqo~~oqrqoqcqhqy~~iqlqoqvqeq~~yqoquqsqoqdawdvd
